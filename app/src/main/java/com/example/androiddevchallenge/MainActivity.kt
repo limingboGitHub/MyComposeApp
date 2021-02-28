d54c517487935477bb693833cbb40a1423665dfe
Limingbo <675683354@qq.com>
2021-02-28 21:31:08 +0800

    finish

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -20,20 +20,23 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.androiddevchallenge.data.Puppy
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
@@ -41,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                MyApp{ lookDetails(it) }
             }
         }
     }
@@ -49,49 +52,70 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(itemClick:(id:Int)->Unit = {}) {
     Surface(
         color = Color.LightGray,
         modifier = Modifier.fillMaxWidth()) {
 
-        val puppies = listOf(
-            "Golden retriever",
-            "Pug",
-            "Teddy",
-            "Shepherd",
-            "Shiba inu",
-            "Poodle")
+        val puppies = getPuppies()
         LazyColumn(horizontalAlignment = Alignment.CenterHorizontally){
             items(puppies){
-                ItemLayout(it)
+                ItemLayout(it,itemClick)
             }
         }
     }
 }
 
 @Composable
-fun ItemLayout(puppy:String){
+fun ItemLayout(puppy: Puppy,itemClick:(id:Int)->Unit){
 
     ConstraintLayout(
         Modifier
             .padding(5.dp)
-            .background(Color.White)) {
-        val (image,text) = createRefs()
+            .clip(RoundedCornerShape(4.dp))
+            .background(Color.White)
+            .clickable { itemClick(puppy.id) }
+            .padding(5.dp)) {
+        val (image,nameText,sexImage,ageText) = createRefs()
 
-        val imagePainter = painterResource(R.drawable.puppy_01)
+        val imagePainter = painterResource(getPuppyImageRes(puppy.name))
         Image(
             imagePainter,"",
             Modifier
-                .width(200.dp)
+                .width(250.dp)
                 .wrapContentHeight()
+//                .padding(10.dp)
+                .clip(RoundedCornerShape(4.dp))
                 .constrainAs(image) {
                     top.linkTo(parent.top, margin = 10.dp)
                 })
 
         Text(
-            puppy,
-            Modifier.constrainAs(text){
+            text = puppy.name,
+            modifier = Modifier.constrainAs(nameText){
                 top.linkTo(image.bottom,margin = 10.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+
+        val sexImagePainter = painterResource(if (puppy.sex == "M") R.drawable.male else R.drawable.female)
+        Image(
+            sexImagePainter,"",
+            Modifier
+                .width(24.dp)
+                .constrainAs(sexImage) {
+                    start.linkTo(nameText.end, margin = 5.dp)
+                    top.linkTo(nameText.top)
+                    bottom.linkTo(nameText.bottom)
+                })
+
+        Text(
+            "${puppy.age} days",
+            Modifier
+                .constrainAs(ageText){
+                top.linkTo(nameText.bottom,margin = 2.dp)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
             })
     }
 
@@ -99,14 +123,26 @@ fun ItemLayout(puppy:String){
 
 }
 
+fun getPuppyImageRes(name:String):Int{
+    return when(name){
+        "Golden retriever" -> R.drawable.golden
+        "Pug" -> R.drawable.pug
+        "Teddy" -> R.drawable.teddy
+        "Shepherd" -> R.drawable.shepherd
+        "Shiba inu" -> R.drawable.shiba
+        "Corgi" -> R.drawable.corgi
+        else -> R.drawable.golden
+    }
+}
+
 private fun getPuppies():List<Puppy>{
     return listOf(
-        Puppy("Golden retriever",10,"F"),
-        Puppy("Pug",10,"F"),
-        Puppy("Teddy",10,"F"),
-        Puppy("Shepherd",10,"F"),
-        Puppy("Shiba inu",10,"F"),
-        Puppy("Poodle",10,"F"),
+        Puppy(0,"Golden retriever",10,"M"),
+        Puppy(1,"Pug",16,"F"),
+        Puppy(2,"Teddy",30,"M"),
+        Puppy(3,"Shepherd",8,"M"),
+        Puppy(4,"Shiba inu",12,"F"),
+        Puppy(5,"Corgi",22,"F"),
     )
 }
 
